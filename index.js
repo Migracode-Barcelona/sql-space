5c2d948ae4ad67adbc57db4ad68054ffcc50dccc
Isar <isarfridriksson@gmail.com>
2023-07-24 15:28:06 +0000

    config for connecting express to pgsql

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { Pool } = require("pg")
+
+const db = new Pool({
+  user: 'postgres',
+  password: 'postgres',
+  host: 'db', // Use the hostname of the database container
+  port: 5432,
+  database: 'db_name'
+});
+
+db.query(`CREATE TABLE IF NOT EXISTS testing(id SERIAL PRIMARY KEY);`)
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
